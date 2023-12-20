4d4211bf2e559e5f09da9561d31b5a81ad725196
jota <juanjose_utn@hotmail.com>
2023-12-20 19:26:10 -0300

    primeros ejercicios hechos

---
+++ js/tp1.js
@@ -0,0 +1,31 @@
+let numero1 = 13;
+let numero2 = 7;
+
+
+let resultado = numero1 + numero2;
+
+document.write('El resultado de la operacion es: ' + (resultado));
+
+resultado = numero1 - numero2;
+
+document.write('<br>El resultado de la operacion es: ' + (resultado));
+
+resultado = numero1 * numero2;
+
+document.write('<br>El resultado de la operacion es: ' + (resultado));
+
+resultado = numero1 / numero2;
+
+document.write('<br>El resultado de la operacion es: ' + (resultado));
+
+let nombre = 'Mateo ';
+let apellido = 'Perez';
+let nombreCompleto = (nombre + apellido);
+
+document.write('<br>Nombre y apellido: ' + nombreCompleto);
+
+let gradosCelsius = 22;
+let gradosFahrenheit = (gradosCelsius * 1.8) + 32;
+
+document.write('<br>Grados Celsius: ' + (gradosCelsius) + '°');
+document.write('<br>El resultado de la conversión a Grados Fahrenheit: ' + (gradosFahrenheit) + '°');
